c65d746d593beee596592d153e5e0681c4e2d2ca
ambi1303 <jhaambikesh555@gmail.com>
2025-04-14 01:14:21 +0530

    Add

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -4,19 +4,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('product/', include('products.urls')),
-    path('accounts/', include('accounts.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('', include('home.urls')),  # Home page
+    path('product/', include('products.urls')),  # Product page
+    path('accounts/', include('accounts.urls')),  # Custom accounts URL
+    path('accounts/', include('allauth.urls')),  # Allauth URLs for authentication (login, signup, etc.)
 ]
 
-
+# Serve media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+# Serve static files in development
 urlpatterns += staticfiles_urlpatterns()
